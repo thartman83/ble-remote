ce2d4622d25154a79facd81653ecd6a3f8a26cdb
Tom Hartman <thomas.lees.hartman@gmail.com>
2024-08-29 00:58:13 -0400

    Forgot two in the last commit

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from '../../../../.emacs.d/tmp/backups/!home!thartman!projects!ble-remote!webapp!src!App.tsx~'
+import App from './App'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
